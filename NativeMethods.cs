c022f7de7d528fffdc41c9c565aeb1cd1584183d
Wobbles Wobbles <Wobbles Wobbles>
2017-06-12 17:02:02 -0400

    Add project files.

---
+++ NativeMethods.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViGEm_360
+{
+    public enum VigemTargetType
+    {
+        Xbox360Wired,
+        XboxOneWired,
+        DualShock4Wired
+    }
+
+    internal static partial class ViGemUm
+    {
+        #region Enums
+
+        public enum VigemError : uint
+        {
+            VIGEM_ERROR_NONE = 0x20000000,
+            VIGEM_ERROR_BUS_NOT_FOUND = 0xE0000001,
+            VIGEM_ERROR_NO_FREE_SLOT = 0xE0000002,
+            VIGEM_ERROR_INVALID_TARGET = 0xE0000003,
+            VIGEM_ERROR_REMOVAL_FAILED = 0xE0000004,
+            VIGEM_ERROR_ALREADY_CONNECTED = 0xE0000005,
+            VIGEM_ERROR_TARGET_UNINITIALIZED = 0xE0000006,
+            VIGEM_ERROR_TARGET_NOT_PLUGGED_IN = 0xE0000007,
+            VIGEM_ERROR_BUS_VERSION_MISMATCH = 0xE0000008,
+            VIGEM_ERROR_BUS_ACCESS_FAILED = 0xE0000009
+        }
+
+        public enum VigemTargetState
+        {
+            VigemTargetNew,
+            VigemTargetInitialized,
+            VigemTargetConnected,
+            VigemTargetDisconnected
+        }
+
+        #endregion
+
+        #region Structs
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct VigemTarget
+        {
+            public uint Size;
+            public uint SerialNo;
+            public VigemTargetState State;
+            public ushort VendorId;
+            public ushort ProductId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Ds4LightbarColor
+        {
+            public byte Red;
+            public byte Green;
+            public byte Blue;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct XusbReport
+        {
+            public ushort wButtons;
+            public byte bLeftTrigger;
+            public byte bRightTrigger;
+            public short sThumbLX;
+            public short sThumbLY;
+            public short sThumbRX;
+            public short sThumbRY;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Ds4Report
+        {
+            byte bThumbLX;
+            byte bThumbLY;
+            byte bThumbRX;
+            byte bThumbRY;
+            ushort wButtons;
+            byte bSpecial;
+            byte bTriggerL;
+            byte bTriggerR;
+        }
+
+        #endregion
+
+        public delegate void VigemXusbNotification(
+            VigemTarget target,
+            byte largeMotor,
+            byte smallMotor,
+            byte ledNumber);
+
+        public delegate void VigemDs4Notification(
+            VigemTarget target,
+            byte largeMotor,
+            byte smallMotor,
+            Ds4LightbarColor lightbarColor);
+
+        public static void VIGEM_TARGET_INIT(
+            [In, Out] ref VigemTarget target)
+        {
+            target.Size = (uint)Marshal.SizeOf(typeof(VigemTarget));
+            target.State = VigemTargetState.VigemTargetInitialized;
+        }
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_init();
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void vigem_shutdown();
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_target_plugin(
+            [In] VigemTargetType type,
+            [In, Out] ref VigemTarget target);
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void vigem_target_unplug(
+            [In, Out] ref VigemTarget target);
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_register_xusb_notification(
+            [In, MarshalAs(UnmanagedType.FunctionPtr)] VigemXusbNotification notification,
+            [In] VigemTarget target);
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_register_ds4_notification(
+            [In, MarshalAs(UnmanagedType.FunctionPtr)] VigemDs4Notification notification,
+            [In] VigemTarget target);
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_xusb_submit_report(
+            [In] VigemTarget target,
+            [In] XusbReport report);
+
+        [DllImport("ViGEmUM.dll", CharSet = CharSet.Unicode, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
+        public static extern VigemError vigem_ds4_submit_report(
+            [In] VigemTarget target,
+            [In] Ds4Report report);
+    }
+}
---
+++ ViGEm360Device.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMPluginInterface;
+using InputMapperIcons;
+
+
+namespace ViGEm_360
+{
+    public class ViGEm360Device : IOutputDevice, IFeedbackDevice
+    {
+        public InputMapperIcon.IMIcons icon { get { return InputMapperIcon.IMIcons.Xbox360; } }
+        public double RumbleBig { get; set; }
+        public double RumbleSmall { get; set; }
+        public double RumbleAux1 { get; set; }
+        public double RumbleAux2 { get; set; }
+
+        private ViGemUm.VigemTarget _target;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler FeedbackReceived;
+        ViGemUm.XusbReport OutputReport = new ViGemUm.XusbReport();
+
+        static ViGEm360Device()
+        {
+            var error = ViGemUm.vigem_init();
+            if (error == ViGemUm.VigemError.VIGEM_ERROR_NONE) return;
+                throw new Exception(string.Format("Init error: {0}", ((ViGemUm.VigemError)error).ToString()));
+        }
+
+        public ViGEm360Device()
+        {
+            ViGemUm.VIGEM_TARGET_INIT(ref _target);
+
+            var error = ViGemUm.vigem_target_plugin(VigemTargetType.Xbox360Wired, ref _target);
+
+            if (error != ViGemUm.VigemError.VIGEM_ERROR_NONE)
+                throw new Exception(string.Format("Init error: {0}", ((ViGemUm.VigemError)error).ToString()));
+
+            ViGemUm.vigem_register_xusb_notification(XusbNotification, _target);
+        }
+
+        public void Dispose()
+        {
+            ViGemUm.vigem_target_unplug(ref _target);
+        }
+
+        private void XusbNotification(ViGemUm.VigemTarget target, byte largeMotor, byte smallMotor, byte ledNumber)
+        {
+            RumbleBig = (double)largeMotor / 255d;
+            RumbleSmall = (double)smallMotor / 255d;
+
+            FeedbackReceived?.Invoke(this, new EventArgs());
+        }
+
+        public bool sendInputs(DeviceState state)
+        {
+            OutputReport.sThumbLX = Convert.ToInt16(state.LX * 32767);
+            OutputReport.sThumbLY = Convert.ToInt16(state.LY * 32767);
+            OutputReport.sThumbRX = Convert.ToInt16(state.RX * 32767);
+            OutputReport.sThumbRY = Convert.ToInt16(state.RY * 32767);
+            OutputReport.bLeftTrigger = (byte)((int)(state.LeftTrigger * 255d));
+            OutputReport.bRightTrigger = (byte)((int)(state.RightTrigger * 255d));
+
+            OutputReport.wButtons = 0x0;
+            //OutputReport.sSpecial = 0;
+
+            if (state.FaceButton1) OutputReport.wButtons |= (ushort)(1 << 5);
+            if (state.FaceButton2) OutputReport.wButtons |= (ushort)(1 << 6);
+            if (state.FaceButton3) OutputReport.wButtons |= (ushort)(1 << 4);
+            if (state.FaceButton4) OutputReport.wButtons |= (ushort)(1 << 7);
+
+            if (state.LeftStickClick) OutputReport.wButtons |= (ushort)(1 << 14);
+            if (state.RightStickClick) OutputReport.wButtons |= (ushort)(1 << 15);
+            if (state.LeftBumper) OutputReport.wButtons |= (ushort)(1 << 8);
+            if (state.RightBumper) OutputReport.wButtons |= (ushort)(1 << 9);
+            if (state.SystemButton1) OutputReport.wButtons |= (ushort)(1 << 12);
+            if (state.SystemButton2) OutputReport.wButtons |= (ushort)(1 << 13);
+            //if (state.HomeButton) OutputReport.bSpecial |= (byte)(1 << 0);
+
+
+            ushort dpad = 0x8;
+            if (!state.DpadUp && !state.DpadDown && !state.DpadLeft && !state.DpadRight) dpad = 0x8;
+            else if (state.DpadUp && !(state.DpadLeft || state.DpadRight)) dpad = 0x0;
+            else if (state.DpadDown && !(state.DpadLeft || state.DpadRight)) dpad = 0x4;
+            else if (state.DpadLeft && !(state.DpadUp || state.DpadDown)) dpad = 0x6;
+            else if (state.DpadRight && !(state.DpadUp || state.DpadDown)) dpad = 0x2;
+            else if (state.DpadUp && state.DpadRight) dpad = 0x1;
+            else if (state.DpadDown && state.DpadRight) dpad = 0x3;
+            else if (state.DpadUp && state.DpadLeft) dpad = 0x7;
+            else if (state.DpadDown && state.DpadLeft) dpad = 0x5;
+
+            OutputReport.wButtons |= dpad;
+
+            try
+            {
+                var error = ViGemUm.vigem_xusb_submit_report(_target, OutputReport);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
